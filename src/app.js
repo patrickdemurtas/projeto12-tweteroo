3951da7b6013da0839456f7fbdafe49cf23a1aca
patrickdemurtas <patrickmurtass@gmail.com>
2023-01-09 03:43:53 -0300

    versao para teste

--- src/app.js
+++ src/app.js
@@ -11,7 +11,7 @@ const PORT = 5000
 
 const users = []
 const tweets = []
-
+let validation = ""
 app.post("/sign-up", (request, response) => {
     users.push(request.body)
     response.send("OK")
@@ -20,36 +20,21 @@ app.post("/sign-up", (request, response) => {
 
 app.post("/tweets", (request, response) => {
 
-    const userName = request.body.username
-    const validation = users.find((u) => u.username === userName)
-    console.log(validation)
-
-    if (validation !== undefined) {
-        tweets.push(request.body.tweet)
+    validation = users.map((u) => u.username)
+    if(validation.includes(request.body.username)){
+        const user = users.find((u) => u.username === request.body.username)
+        const avatar = user.avatar
+        const tweet = {username: request.body.username, avatar: avatar, tweet: request.body.tweet}
+        tweets.push(tweet)
         response.send("OK")
-        console.log(tweets)
-    } else if(validation === undefined){
-       response.send("UNAUTHORIZED")
+    } else{
+        response.send("UNAUTHORIZED")
     }
+   
 })
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+app.get("/tweets", (request, response) => {
+   let lastTen = tweets.slice((tweets.length - 11), tweets.length)
+})
 
 app.listen(PORT, () => console.log(`teste porta:${PORT}`))
