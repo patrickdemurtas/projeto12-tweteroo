4ba7373d2a33531e0cc81fe87eedbc5351af6faf
patrickdemurtas <patrickmurtass@gmail.com>
2023-01-09 02:25:36 -0300

    sign-up e tweets

--- src/app.js
+++ src/app.js
@@ -12,10 +12,25 @@ const PORT = 5000
 const users = []
 const tweets = []
 
-app.post('/sign-up', (request, response) => {
-   users.push(request.body)
-   response.send("OK")
-   console.log(users)
+app.post("/sign-up", (request, response) => {
+    users.push(request.body)
+    response.send("OK")
+    console.log(users)
+})
+
+app.post("/tweets", (request, response) => {
+
+    const userName = request.body.username
+    const validation = users.find((u) => u.username === userName)
+    console.log(validation)
+
+    if (validation !== undefined) {
+        tweets.push(request.body.tweet)
+        response.send("OK")
+        console.log(tweets)
+    } else if(validation === undefined){
+       response.send("UNAUTHORIZED")
+    }
 })
 
 
